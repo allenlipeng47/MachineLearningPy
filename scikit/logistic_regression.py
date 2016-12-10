347cbc76cb702feff6d8d79b91725c4aeb8634aa
allenlipeng47 <allen.lipeng47@gmail.com>
2016-12-09 22:34:11 -0800

    Added logistic regression, plus outlining the boundary and resion.

---
+++ scikit/logistic_regression.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model, datasets
+
+X = np.array([
+    [-3, -2],
+    [-3, 1],
+    [0, 4],
+    [8, 9],
+    [8, 10],
+    [3, 4],
+    [11, 11],
+    [15, 6],
+    [13, 10],
+    [13, 19],
+    [23, -1],
+    [25, -2],
+    [-2, -3],
+    [3, 2],
+    [10, 9],
+    [15, 10],
+    [20, -1],
+    [25, -10]
+])
+
+Y = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2])
+
+logreg = linear_model.LogisticRegression(C=1e5)
+
+# we create an instance of Neighbours Classifier and fit the data.
+logreg.fit(X, Y)
+
+print logreg.predict(np.array([[10, 5]]))
+
+h = .02  # step size in the mesh
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1, figsize=(4, 3))
+plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=Y, edgecolors='k', cmap=plt.cm.Paired)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
